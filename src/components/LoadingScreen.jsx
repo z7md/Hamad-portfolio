3fbddd0701640275093d3004c99f5920743873b1
z7md <z7mdd90@gmail.com>
2025-02-25 15:21:40 +0300

    x

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Hello Wolrd />";
+  const fullText = "<Hello World />";
 
   useEffect(() => {
     let index = 0;
