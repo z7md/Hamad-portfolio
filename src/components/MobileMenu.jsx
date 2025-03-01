2c2e9bb8c8a8b98fafff6abb2b27b38f40a961f0
z7md <z7mdd90@gmail.com>
2025-03-01 20:14:36 +0300

    xl

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -15,7 +15,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     >
       <button
         onClick={() => setMenuOpen(false)}
-        className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+        className="absolute top-6 right-6 text-white text-5xl focus:outline-none cursor-pointer"
         aria-label="Close Menu"
       >
         &times;
