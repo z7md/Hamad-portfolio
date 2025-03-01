9954a51bbc45b54f6570ee817a8fe5c19ca322fa
z7md <z7mdd90@gmail.com>
2025-03-01 19:44:40 +0300

    layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
   }, [menuOpen]);
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-      <div className="max-w-5xl mx-auto px-12">
+      <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             {" "}
@@ -14,7 +14,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
           </a>
 
           <div
-            className={`w-7 h-5 relative cursor-pointer z-100 md:hidden text-2xl ${menuOpen ? "hidden" : ""}`}
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
             onClick={() => setMenuOpen((prev) => !prev)}
           >
             &#9776;
