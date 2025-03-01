bd1aef6287f9928608bb29483906d601f8ff8eb4
z7md <z7mdd90@gmail.com>
2025-03-01 20:08:19 +0300

    la

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
   }, [menuOpen]);
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-      <div className="max-w-5xl mx-auto px-12">
+      <div className="max-w-3xl mx-auto px-12">
         <div className="flex justify-between w-full items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             {" "}
