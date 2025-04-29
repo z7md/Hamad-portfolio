4eb46fac03f5042dd5a2a42fc4a05fd3f22b34b1
z7md <z7mdd90@gmail.com>
2025-04-29 19:47:47 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Hamad-portfolio",
+  base: "/",
 });
